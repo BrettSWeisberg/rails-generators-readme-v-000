953d7b2892321ddd6c0c547605ea07f3056ca9b7
Brett Weisberg <weisberg.brett@gmail.com>
2018-09-13 21:54:46 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180913215310) do
+ActiveRecord::Schema.define(version: 20180913215434) do
 
   create_table "accounts", force: :cascade do |t|
     t.string   "name"
